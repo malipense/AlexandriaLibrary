b88e7616d0ad617fa8d726954e08a739ee513fb0
Eduardo Malipense <eduardo.malipense@gmail.com>
2024-04-04 10:01:36 -0300

    add arp poisoning project

---
+++ Database/networking/arp-poisoning/C#/arp-poisoning/arp-poisoning-tool/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//string target = args[0];
+
+NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces().Where(n => n.OperationalStatus == OperationalStatus.Up)
+    .ToArray();
+
+PhysicalAddress mac = nics[1].GetPhysicalAddress();
+
+using(Socket socket = new Socket(SocketType.Rdm, ProtocolType.Unspecified))
+{
+
+}
+
+Console.ReadKey();
